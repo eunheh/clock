c46c35864e505117724219ecd08e7fa68dc80bb0
Grace Lee <eunheh@gmail.com>
2016-10-11 16:41:08 -0400

    color and hex works

--- main.js
+++ main.js
@@ -6,10 +6,25 @@ function formatTime() {
 
   if (secs < 10) { secs = "0" + secs; }
   if (mins < 10) { mins = "0" + mins; }
-  if (hrs <10 ) { hrs = "0" + hrs; }
+  if (hrs < 10) { hrs = "0" + hrs; }
 
   var newTime = document.getElementById("clock");
   newTime.innerHTML = (hrs + ":" + mins + ":" + secs);
 }
-
 setInterval(formatTime, 1000);
+
+function formatHex() {
+  var nowTime = new Date();
+  var hrs = nowTime.getHours();
+  var mins = nowTime.getMinutes();
+  var secs = nowTime.getSeconds();
+
+  if (secs < 10) { secs = "0" + secs; }
+  if (mins < 10) { mins = "0" + mins; }
+  if (hrs < 10) { hrs = "0" + hrs; }
+
+  var newHex = document.getElementById("hex");
+  newHex.style.backgroundColor = ("#" + hrs + mins + secs);
+  newHex.innerHTML = ("#" + hrs + mins + secs);
+}
+setInterval(formatHex, 1000);
